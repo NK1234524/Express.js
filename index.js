88e12dd4207a9ca81e79fe763b7955cb48247233
Nishant kumar <nishant201kashyap@gmail.com>
2025-07-03 15:37:05 +0530

    Update

--- index.js
+++ index.js
@@ -6,10 +6,34 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-app.use( (req, res) => {
+app.get( "/",(req, res) => {
   console.log("Request recieved");
-   res.send({
-    name : "Apple",
-    Color : "Red"
-   }); 
-})
+   res.send("The Fruit list is here "); 
+});
+
+
+app.get( "/apple",(req, res) => {
+  console.log("Request recieved");
+   res.send("The Fruit is apple"); 
+});
+
+app.get("/mango",(req,res)=>{
+    console.log("Request recieved");
+    res.send("The Fruit is mango");
+});
+
+app.get("/Orange",(req,res)=>{
+    console.log("Request recieved");
+    res.send("The Fruit is Orange")
+});
+
+app.post("/",(req,res)=>{
+    console.log("Request recieved");
+    res.send("The response is post type");
+});
+
+app.use((req,res)=>{
+    console.log("Request recieved");
+    res.send("The response is not acheived");
+});
+
